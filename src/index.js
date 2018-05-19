06599232b8a4ec4635c0cdb44b74029d072e7b68
Jon <jbvaldesuso@gmail.com>
2018-05-18 20:42:45 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Product from './components/Product';
+import {Product} from './components/Product';
 
 ReactDOM.render(
   <Product
